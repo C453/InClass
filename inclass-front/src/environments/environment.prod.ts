8a246904e0ee02932e1a0e4aec52ce3570747089
Case Wright <wrigh404@purdue.edu>
2019-03-08 16:06:51 -0500

    set prod wss vars

--- inclass-front/src/environments/environment.prod.ts
+++ inclass-front/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   token_auth_config: {
-    apiBase: 'https://project-inclass-backend.herokuapp.com'
+    apiBase: 'https://project-inclass-backend.herokuapp.com',
+    socketBase: 'ws://project-inclass-backend.herokuapp.com/'
   }
 };
