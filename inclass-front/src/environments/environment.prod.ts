5a0b456aad28b345abc9b0b7060f18fe3b097665
Case Wright <wrigh404@purdue.edu>
2019-03-22 20:54:14 -0400

    create defects urls

--- inclass-front/src/environments/environment.prod.ts
+++ inclass-front/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   token_auth_config: {
-    apiBase: 'https://project-inclass-backend.herokuapp.com',
-    socketBase: 'wss://project-inclass-backend.herokuapp.com/cable'
+    apiBase: 'https://project-inclass-backend-bugs.herokuapp.com',
+    socketBase: 'wss://project-inclass-backend-bugs.herokuapp.com/cable'
   }
 };
